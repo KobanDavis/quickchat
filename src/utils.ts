8a3b50cbfd3070df6da9929fdc63d37577c5067e
Koban Davis <kobandavis@gmail.com>
2023-03-13 09:20:29 +0000

    add timestamps

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,17 @@ function sleep(ms: number): Promise<void> {
 	return new Promise((r) => setTimeout(r, ms))
 }
 
-const log = {
-	info: (...logs: any[]) => console.log('\x1b[38;5;6m[INFO]\x1b[0m', ...logs),
-	error: (...logs: any[]) => console.log('\x1b[38;5;1m[ERROR]\x1b[0m', ...logs)
+function timestamp() {
+	let date = ''
+	const time = new Date()
+	date += time.getHours().toString().padStart(2, '0') + ':'
+	date += time.getMinutes().toString().padStart(2, '0') + ':'
+	date += time.getSeconds().toString().padStart(2, '0')
+	return date
 }
-export { sleep, log }
+
+const log = {
+	info: (...logs: any[]) => console.log(`[${timestamp()}]`, '\x1b[38;5;6m[INFO]\x1b[0m', ...logs),
+	error: (...logs: any[]) => console.log(`[${timestamp()}]`, '\x1b[38;5;1m[ERROR]\x1b[0m', ...logs)
+}
+export { sleep, log, timestamp }
